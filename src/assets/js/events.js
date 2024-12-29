779adc5c5fc3147b013295d425e898826b88722a
Hajar elkhazri <elkhazrihajar373@gmail.com>
2024-12-29 11:45:35 +0100

    message

--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -67,8 +67,7 @@ window.addEventListener( 'load', () => {
             let roomLink = `${ location.origin }?room=${ roomName.trim().replace( ' ', '_' ) }_${ helpers.generateRandomString() }`;
 
             //show message with link to room
-            document.querySelector( '#room-created' ).innerHTML = `Room successfully created. Click <a href='${ roomLink }'>here</a> to enter room. 
-                Share the room link with your partners.`;
+            document.querySelector( '#room-created' ).innerHTML = ` <a href='${ roomLink }'> <button > JOIN </button> </a>`;
 
             //empty the values
             document.querySelector( '#room-name' ).value = '';
